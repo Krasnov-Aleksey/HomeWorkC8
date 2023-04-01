748f88c3e108901624256cd3c7b098f6fabf9e9f
aleksey.krasnov <krasnov.80@mail.ru>
2023-04-01 13:44:00 +0300

    Task54 done

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -17,11 +17,56 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
+void PowMatrix(int[] sortMatrix, int[,] matrix, int pow) // cтрока матрицы => массив
+{
+    for (int i = 0; i < sortMatrix.Length; i++)
+    {
+        sortMatrix[i] = matrix[pow, i];
+    }
+}
+
+void SortMatrix(int[] sortMatrix, int[,] matrix) //сортировка массива по убыванию
+{
+    int temp = 0;
+    for (int i = 0; i < sortMatrix.Length; i++)
+    {
+        for (int j = 0; j < sortMatrix.Length; j++)
+        {
+            if (sortMatrix[i] > sortMatrix[j])
+            {
+                temp = sortMatrix[i];
+                sortMatrix[i] = sortMatrix[j];
+                sortMatrix[j] = temp;
+            }
+        }
+    }
+}
+
+void NewMatrix(int[] sortMatrix, int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        PowMatrix(sortMatrix, matrix, i);
+        SortMatrix(sortMatrix, matrix);
+        for (int j = 0; j < sortMatrix.Length; j++)
+        {
+            matrix[i, j] = sortMatrix[j];
+        }
+
+
+    }
+
+}
+
 Console.Clear();
 Console.Write("Введите размеры двумерного массива: ");
 int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[1]];
+int[] sortMatrix = new int[size[1]];
 InputMatrix(matrix);
 Console.WriteLine("Начальный массив:");
 PrintMatrix(matrix);
-Console.WriteLine();
+Console.WriteLine("Новый массив:");
+NewMatrix(sortMatrix, matrix);
+PrintMatrix(matrix);
+
