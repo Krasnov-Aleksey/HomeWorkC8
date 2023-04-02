8e468ebf0c127f89d2e17e9c8394ba677a1b0d6b
aleksey.krasnov <krasnov.80@mail.ru>
2023-04-02 11:04:54 +0300

    Task56 done

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -17,15 +17,40 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
+int SumElement(int[,] matrix,int numLine) //  Сумма элементов
+{
+    int sum=0;
+    for(int i=0; i<matrix.GetLength(1); i++)
+    {
+        sum=sum+matrix[numLine,i];
+    }
+    return sum;
+}
+
+int MinSumElement(int[,] matrix) //минимальная строка суммы элементов
+{
+    int sum=0;
+    int min=SumElement(matrix,0);
+    int index=0;
+    for(int i=1;i<matrix.GetLength(0);i++)
+    {
+        sum=SumElement(matrix,i);
+        if(min>sum)
+        {
+            min=sum;
+            index=i;
+        }
+    }
+    return index+1;
+}
+
 Console.Clear();
 Console.Write("Задайте прямоугольный двумерный массив: ");
-//int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
 int size=Convert.ToInt32(Console.ReadLine());
 int[,] matrix = new int[size, size];
 InputMatrix(matrix);
 Console.WriteLine("Начальный массив");
 PrintMatrix(matrix);
-Console.WriteLine("Конечный массив");
-PrintMatrix(matrix);
-
-
+int minLine;
+minLine=MinSumElement(matrix);
+Console.WriteLine($"Строка с наименьшей суммой элементов {minLine}");
